1bb94199a0da8b955d1f6b0477a28d200d9a2f94
Chariemo <charleyschen@gmail.com>
2018-03-04 17:07:02 +0800

    generator example

---
+++ generator.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n += 1
+    return 'done'
+
+g = fib(6)
+while True:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value: ', e.value)
+        break
+
